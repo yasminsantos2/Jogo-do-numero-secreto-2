5738a9c2003ef0f56eb527ac40f95dde37079b17
Yasmin Santos <yasminsantos.eng@gmail.com>
2025-06-16 19:44:29 -0300

    style: ajuste de quebra de texto

--- app.js
+++ app.js
@@ -8,18 +8,12 @@ function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
-
-
-
-
 }
 
 function exibirMensagemInicial() {
     exibir
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
-
-    
 }
  
 exibirMensagemInicial();
