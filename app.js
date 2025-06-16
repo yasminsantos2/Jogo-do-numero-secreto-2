d24b21a52657e86abeb613503f21feb85a4ce8d4
Yasmin Santos <yasminsantos.eng@gmail.com>
2025-06-16 19:35:03 -0300

    teste: quebra de texto

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 
-    
+
 }
 
 function exibirMensagemInicial() {
