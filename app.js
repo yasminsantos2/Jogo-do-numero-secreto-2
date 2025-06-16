f3b1041b3147831720218e3e161cdd6fecd67c29
Yasmin Santos <yasminsantos.eng@gmail.com>
2025-06-16 19:37:49 -0300

    teste: quebra de texto

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    
 
 
 }
