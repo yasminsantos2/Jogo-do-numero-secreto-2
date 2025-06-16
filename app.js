f2f0782c751e5aba1380292979a2cf66fd820451
Yasmin Santos <yasminsantos.eng@gmail.com>
2025-06-16 19:31:37 -0300

    test: quebra de texto

--- app.js
+++ app.js
@@ -13,13 +13,14 @@ function exibirMensagemInicial() {
     exibir
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
-    
+
 }
 
 exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
+    
     
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
